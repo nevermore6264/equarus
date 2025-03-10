410dc5f74d61869fc6d8b40125586b67c9ae616a
David <nevermore6264@example.com>
2025-03-10 22:47:08 +0700

    add

---
+++ components/Home/Partner.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="careers">
+    <p class="title">Partner</p>
+    <div class="underline-container">
+      <div class="underline" style="background-color: #00dbf4"></div>
+      <i class="pi pi-arrow-down-left" style="color: black"></i>
+    </div>
+    <p class="subtitle"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Partner",
+};
+</script>
+
+<style scoped>
+.careers {
+  padding: 89.5px;
+}
+
+.title {
+  font-size: 48px;
+  font-family: "Inter", sans-serif;
+}
+
+.subtitle {
+  font-size: 24px;
+  color: #343434;
+  margin-bottom: 100px;
+  font-family: "Inter", sans-serif;
+}
+
+.underline-container {
+  display: flex;
+  align-items: center;
+}
+
+.underline {
+  flex: 1;
+  height: 2px;
+  background-color: #00dbf4;
+}
+
+.pi-arrow-down-left {
+  margin-left: 0.5rem;
+  font-size: 1.5rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Nuxt />
+    <Partner />
     <Contact />
     <Careers />
     <Footer />
@@ -8,12 +9,14 @@
 </template>
 
 <script>
+import Partner from "~/components/Home/Partner.vue";
 import Contact from "~/components/Home/Contact.vue";
 import Careers from "~/components/Home/Careers.vue";
 import Footer from "~/components/Layout/Footer.vue";
 
 export default {
   components: {
+    Partner,
     Contact,
     Careers,
     Footer,
