db723a58224b647be749c44dfa3e5c30b3e26796
David <nevermore6264@example.com>
2025-03-16 01:29:28 +0700

    add

--- components/Home/Mission.vue
+++ components/Home/Mission.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="mission">
     <div class="mission-content">
-      <h1>WHEREVER VIETNAMESE GOODS GO, THE BORDER OF VIETNAM FOLLOWS</h1>
-      <p>
-        Equarus Solutions tập trung vào các thị trường thương mại điện tử quốc
-        tế lớn, bao gồm Hoa Kỳ (US), Vương quốc Anh (UK), Liên minh châu Âu
-        (EU), Úc (AU) và Canada.
-      </p>
-      <p>
-        Trong đó Hoa Kỳ (US) chiếm từ trọng chính nhờ quy mô và tiềm năng tiêu
-        dùng lớn.
-      </p>
-      <button class="join-button">JOIN OUR TEAM</button>
+      <div class="left-side">
+        <h1>WHEREVER VIETNAMESE GOODS GO, THE BORDER OF VIETNAM FOLLOWS</h1>
+      </div>
+      <div class="right-side">
+        <p>
+          Equarus Solutions tập trung vào các thị trường thương mại điện tử quốc
+          tế lớn, bao gồm Hoa Kỳ (US), Vương quốc Anh (UK), Liên minh châu Âu
+          (EU), Úc (AU) và Canada.
+        </p>
+        <p>
+          Trong đó Hoa Kỳ (US) chiếm từ trọng chính nhờ quy mô và tiềm năng tiêu
+          dùng lớn.
+        </p>
+        <button class="join-button">JOIN OUR TEAM</button>
+      </div>
     </div>
   </div>
 </template>
@@ -29,30 +33,49 @@ export default {
 }
 
 .mission-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   margin: 0 auto;
 }
 
-.mission-content h1 {
-  font-size: 2.5rem;
-  color: #2c3e50;
-  margin-bottom: 1.5rem;
+.left-side {
+  flex: 1;
+  text-align: left;
+  padding-right: 2rem;
 }
 
-.mission-content p {
-  font-size: 1.2rem;
-  color: #4a5568;
-  line-height: 1.6;
+.left-side h1 {
+  font-size: 45.33px;
+  color: #000000;
   margin-bottom: 1.5rem;
+  line-height: 1.6;
+  font-weight: 300;
+  font-family: "Krona One", sans-serif;
+  vertical-align: top;
+}
+
+.right-side {
+  flex: 1;
+  text-align: left;
+}
+
+.right-side p {
+  font-size: 29.33px;
+  color: #000000;
+  line-height: 1.6;
+  font-family: "Inter", sans-serif;
 }
 
 .join-button {
-  padding: 16px 44px;
+  margin-top: 30px;
+  padding: 27.5px 100px;
   font-size: 15px;
   font-family: "Lexend Zetta", sans-serif;
   color: #000000;
   background-color: #d9d9d9;
   border: none;
-  border-radius: 30px;
+  border-radius: 40px;
   cursor: pointer;
   text-transform: uppercase;
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,6 +49,10 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap',
         },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Krona+One&display=swap'
+        }
       ]
     }
   }
