153364d08d1b2ba73578149fcc06c17a6981b58a
David <nevermore6264@example.com>
2025-04-12 17:43:00 +0700

    Add

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
     <History />
     <Target />
     <Partner />
-    <Contact />
+    <!-- <Contact /> -->
     <Careers />
     <Footer />
   </div>
@@ -30,7 +30,7 @@ import Culture from "~/components/Home/Culture.vue";
 import History from "~/components/Home/History.vue";
 import Target from "~/components/Home/Target.vue";
 import Partner from "~/components/Home/Partner.vue";
-import Contact from "~/components/Home/Contact.vue";
+// import Contact from "~/components/Home/Contact.vue";
 import Careers from "~/components/Home/Careers.vue";
 import Footer from "~/components/Layout/Footer.vue";
 
@@ -47,7 +47,7 @@ export default {
     History,
     Target,
     Partner,
-    Contact,
+    // Contact,
     Careers,
     Footer,
   },
